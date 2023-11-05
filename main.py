16ba2fb453be6030c69943c350e90de3f19c4e8b
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-05 16:36:41 +0200

    додала дані по виведенню інформації та порівння щодо того, хто старший і на скільки

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Завдання 1
 # До вже реалізованого класу «Людина» додайте конструктор та необхідні перевантажені методи.
 
+from datetime import datetime
 class People:
     def __init__(self, full_name, date_of_birth, contact_phone_number, city, country, home_address):
         self.full_name = full_name
@@ -28,16 +29,52 @@ class People:
     def get_home_address(self):
         return self.home_address
 
+    def __eq__(self, other):
+        # Перевизначення оператора ==
+        return self.full_name == other.full_name
+
+    def __lt__(self, other):
+        date1 = datetime.strptime(self.date_of_birth, "%Y-%m-%d")
+        date2 = datetime.strptime(other.date_of_birth, "%Y-%m-%d")
+
+    def __sub__(self, other):
+        date1 = datetime.strptime(self.date_of_birth, "%Y-%m-%d")
+        date2 = datetime.strptime(other.date_of_birth, "%Y-%m-%d")
+        difference = date1 - date2
+        years = difference.days // 365  # Оцінка років
+        return years
+
 # Створення екземпляра класу
-person = People("Шевченко Т.Г.", "2010-03-09", "0661234567", "Київ",
+person1 = People("Шевченко Т.Г.", "2020-03-09", "0661234567", "Київ",
                "Україна", "вул. Листопадна буд. 4")
-
-# Виведення інформації про Людину
-print(f"ПІБ: {person.get_full_name()}")
-print(f"Дата народження: {person.get_date_of_birth()}")
-print(f"Контактний телефон: {person.get_contact_phone_number()}")
-print(f"Місто: {person.get_city()}")
-print(f"Країна: {person.get_country()}")
-print(f"Домашня адреса: {person.get_home_address()}")
+person2 = People("Котляревський І.П.", "2005-01-01", "0998765432", "Львів",
+                 "Україна", "вул. Незалежності буд. 10")
 
 
+print(f"ПІБ: {person1.get_full_name()}")
+print(f"Дата народження: {person1.get_date_of_birth()}")
+print(f"Контактний телефон: {person1.get_contact_phone_number()}")
+print(f"Місто: {person1.get_city()}")
+print(f"Країна: {person1.get_country()}")
+print(f"Домашня адреса: {person1.get_home_address()}")
+
+
+print(f"ПІБ: {person2.get_full_name()}")
+print(f"Дата народження: {person2.get_date_of_birth()}")
+print(f"Контактний телефон: {person2.get_contact_phone_number()}")
+print(f"Місто: {person2.get_city()}")
+print(f"Країна: {person2.get_country()}")
+print(f"Домашня адреса: {person2.get_home_address()}")
+
+if person1 == person2:
+    print("Обидві людини мають однакове ім'я.")
+else:
+    print("Люди мають різні імена.")
+
+if person1 < person2:
+    print("Людина 1 молодший за Людину 2.")
+else:
+    print("Людина 1 старший Людина 2.")
+
+difference = abs(person1 - person2)
+print(f"Різниця в : {difference} роках")
