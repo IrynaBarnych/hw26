a28a8af3966eded6ec6016eacd196df486c3665c
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-05 20:22:32 +0200

    додала кількість переглядів за рік

--- main.py
+++ main.py
@@ -5,10 +5,11 @@
 # для інших операцій. Використовуйте механізм перевантаження методів.
 
 class Website:
-    def __init__(self, name, adres, describe):
+    def __init__(self, name, adres, describe, watch):
         self.name = name
         self.adres = adres
         self.describe = describe
+        self.watch = watch
 
     def get_name(self):
         return self.name
@@ -18,17 +19,21 @@ class Website:
 
     def get_describe(self):
         return self.describe
+    def get_watch(self):
+        return self.watch
 
-website1 = Website("Вікіпедія", "https://uk.wikipedia.org", "Вікіпе́дія — загальнодоступна вільна багатомовна онлайн-енциклопедія")
-website2 = Website("YouTube", "https://www.youtube.com/", "YouTube — популярний відеохостинг, що надає послуги розміщення відеоматеріалів")
+website1 = Website("Вікіпедія", "https://uk.wikipedia.org", "Вікіпе́дія — загальнодоступна вільна багатомовна онлайн-енциклопедія", 1000000)
+website2 = Website("YouTube", "https://www.youtube.com/", "YouTube — популярний відеохостинг, що надає послуги розміщення відеоматеріалів", 1000000000)
 
 print(f"Назва вебсайту: {website1.get_name()}")
 print(f"Адреса вебсайту: {website1.get_adres()}")
 print(f"Опис вебсайту: {website1.get_describe()}")
+print(f"Кількість перегляду за рік: {website1.get_watch()}")
 
 print(f"Назва вебсайту: {website2.get_name()}")
 print(f"Адреса вебсайту: {website2.get_adres()}")
 print(f"Опис вебсайту: {website2.get_describe()}")
+print(f"Кількість перегляду за рік: {website2.get_watch()}")
 
 
 
