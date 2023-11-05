9d6bd3fe1d62d1d195263ebc02bb23ed561dadc2
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-05 15:49:31 +0200

    шаблон

---
+++ main.py
@@ -0,0 +1,22 @@
+# Завдання
+#
+
+class:
+    def __init__(self):
+        self. =
+        self. =
+        self. =
+        self. =
+
+
+    def print_info(self):
+        print(f": {}, : {}, : {}, : {e}")
+
+my_info1 = ("", "", "", "")
+my_info1.print_info()
+
+my_info2 =("", "", "", "")
+my__info2.print_info()
+
+
+
